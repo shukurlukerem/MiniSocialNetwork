bbfad143136ea44ad447b38863b354d8849c1a79
Karam Shukurlu <shukurlukerem1@gmail.com>
2025-07-16 09:07:14 +0400

    done

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
-from app.api.v1.routes import auth_routes
-from app.api.v1.routes import user_routes
+from api.v1.routes import auth_routes
+from api.v1.routes import user_routes
 from app.core.config import Base, engine
 from fastapi_cache import FastAPICache
 #from fastapi_cache.backends.redis import RedisBackend
 import aioredis
-from app.core.rate_limiter import init_rate_limiter
-from app.api.v1.routes import post_routers 
+from core.rate_limiter import init_rate_limiter
+from api.v1.routes import post_routers 
+from fastapi import FastAPI, Request
+from core.log import logger
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI(
@@ -31,3 +33,10 @@ def test():
 @app.on_event("startup")
 async def startup():
     await init_rate_limiter()
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    logger.info(f"Incoming request: {request.method} {request.url}")
+    response = await call_next(request)
+    logger.info(f"Response status: {response.status_code}")
+    return response
